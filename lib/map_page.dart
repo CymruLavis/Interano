dd94fb5096ffbf8ce88dedc5b74515831390edb9
CymruLavis <117460546+CymruLavis@users.noreply.github.com>
2022-11-28 11:56:06 -0500

    trying to fix the pop up menu buttons

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -3,6 +3,13 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'filters.dart';
+import 'models/colourSwatch.dart';
+
+enum MenuItem {
+  filter,
+  settings,
+  logout,
+}
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -30,47 +37,31 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Maps Sample App'),
           backgroundColor: Colours().getAppBarColor(),
           actions: [
-            PopupMenuButton(
-                // add icon, by default "3 dot" icon
-                icon: Icon(Icons.menu),
-                itemBuilder: (context) {
-                  return [
-                    PopupMenuItem<int>(
-                      value: 0,
-                      child: ListTile(
-                          leading: Icon(Icons.filter), title: Text("Filters")),
-                    ),
-                    PopupMenuItem<int>(
-                      value: 1,
-                      child: ListTile(
-                          leading: Icon(Icons.settings),
-                          title: Text("Settings")),
-                    ),
-                    PopupMenuItem<int>(
-                      value: 2,
-                      child: ListTile(
-                          leading: Icon(Icons.logout), title: Text("Logout")),
-                    ),
-                  ];
-                },
+            PopupMenuButton<MenuItem>(
                 onSelected: (value) {
-                  //Change screens
-                  if (value == 0) {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const FiltersPage()),
-                    );
-                  } else if (value == 1) {
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //       builder: (context) => const SettingsPage()),
-                    // );
-                  } else if (value == 2) {
-                    //Log out and go to sign in page
-                  }
-                }),
+                  if (value == MenuItem.filter) {
+                    print("Filter page");
+                    // With the navigator option, the button is not being recognized as a button
+                    // Navigator.of(context).push(MaterialPageRoute(
+                    //   builder: (context) => const FiltersPage(),
+                    // ));
+                  } else if (value == MenuItem.settings) {
+                  } else if (value == MenuItem.logout) {}
+                },
+                itemBuilder: (context) => [
+                      PopupMenuItem(
+                        child: Text("Filter"),
+                        value: MenuItem.filter,
+                      ),
+                      PopupMenuItem(
+                        child: Text("Settings"),
+                        value: MenuItem.settings,
+                      ),
+                      PopupMenuItem(
+                        child: Text("Logout"),
+                        value: MenuItem.logout,
+                      ),
+                    ])
           ],
         ),
         body: GoogleMap(
